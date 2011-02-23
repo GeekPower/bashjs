ab79b851399d053264d194911a3d416a9434249d
root <root@sauron.sauron>
2011-02-23 22:11:18 +0200

    Exemplu de utilizare

--- index.php
+++ index.php
@@ -4,9 +4,6 @@
 
         <script src="/lib/jquery-1.5.1.min.js"></script>
         <script src="/lib/bashjs.loader.js"></script>
-        <script>
-            var bashJS = new BashJS;
-        </script>
 
     </head>
 
@@ -14,7 +11,6 @@
         <script>
             $(document).ready( function()
             {
-                bashJS.Start();
             } );
             
         </script>
