18c5ee0659750f2229bb437a515d12818e9196be
daniel <lucadanielcostin@gmail.com>
2011-02-23 22:30:35 +0200

    fix errors in vfs.js

--- vfs/vfs.js
+++ vfs/vfs.js
@@ -2,12 +2,14 @@
  * mount a pseudo device file to a mount point
  */
 
+mnt_array = [];
+
 vfs = {
-	var mnt_array = [];
+	mnt_array: '',
 
 	'mount': function mount(device, mount_point, options) {
 		mnt_array.push(new Mounter(device, mount_point, options));
-	}
+	},
 
 	'umount': function umount(mount_point) {
 		for(var i = 0; i < mnt_array; i++) {
@@ -15,35 +17,35 @@ vfs = {
 				delete mnt_array[i];
 			}
 		}
-	}
+	},
 
 	'open': function open(filename) {
 		/* do nothing, successfully */
-	}
+	},
 
 	'close': function close(fd) {
 		/* do nothing, successfully */
-	}
+	},
 
 	'fcntl': function fcntl(fd, options) {
 		/* do nothing, successfully */
-	}
+	},
 
 	'opendir': function opendir(fd) {
 		/* do nothing, successfully */
-	}
+	},
 
 	'readdir': function readdir(fd) {
 		/* do nothing, successfully */
-	}
+	},
 
 	'read': function read(fd, size) {
 		/* do nothing, successfully */
-	}
+	},
 
 	'write': function write(fd, buffer) {
 		/* do nothing, successfully */
-	}
+	},
 
 	'lseek': function lseek(fd, buffer) {
 		/* do nothing, successfully */
