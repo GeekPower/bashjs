c13bea1987ff51bab0dc1428b8ae132c978c9206
root <root@sauron.sauron>
2011-02-23 21:34:21 +0200

    add jquery, bashloader

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+
+    <head>
+
+        <script src="/lib/jquery-1.5.1.min.js"></script>
+        <script src="/lib/bashjs.loader.js"></script>
+        <script>
+            var bashJS = new BashJS;
+        </script>
+
+    </head>
+
+    <body>
+        <script>
+            $(document).ready( function()
+            {
+                bashJS.Start();
+            } );
+            
+        </script>
+    </body>
+</html>
