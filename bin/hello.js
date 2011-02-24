bba0a3576e0b7209776ba32622f5e70118e1da9b
daniel <lucadanielcostin@gmail.com>
2011-02-24 02:36:43 +0200

    ok

--- bin/hello.js
+++ bin/hello.js
@@ -1,10 +1,19 @@
 function hello()
 {
-    this.start = function()
+    this.start = function( argv )
     {
         bashjs.bash.write( 'world' );
+
+        if ( argv.length > 0 )
+        {
+            bashjs.bash.write( 'Arguments: ' );
+            for ( i = 0 ; i < argv.length ; i++ )
+            {
+                bashjs.bash.write( argv[i] );
+            }
+        }
     }
 }
 
 var temp_val = new hello();
-temp_val.start();
+temp_val.start( bashjs_bash.command_argv );
